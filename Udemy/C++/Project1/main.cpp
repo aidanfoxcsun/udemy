0ba03fdff82f509e21137aa7f2d85b6ed7d1acc3
aidanfoxcsun <aidan.fox.220@my.csun.edu>
2023-06-01 13:49:37 -0700

    Added Files
    Moved files from my desktop workspace to github repo.

---
+++ Udemy/C++/Project1/main.cpp
@@ -0,0 +1,16 @@
+// Programmer: Aidan Fox
+
+#include <iostream>
+
+// first project in Udemy course
+// added some external knowledge from previously working with the language.
+
+
+int main() {
+	int favoriteNumber;
+
+	std::cout << "Enter a number between 1 and 100" << std::endl;
+	std::cin >> favoriteNumber;
+	std::cout << "Wow! " << favoriteNumber << " is my favorite number too!" << std::endl;
+	return 0;
+}
