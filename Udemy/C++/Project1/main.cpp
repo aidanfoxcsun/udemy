52b7360783265b563847b3a1fb9fe8fa88a6f850
aidanfoxcsun <aidan.fox.220@my.csun.edu>
2023-06-02 11:12:27 -0700

    Completed Challenge for Section 4
    Completed the challenge for section 4. Admittedly , not much has changed in the code, because I already had this functionality.

--- Udemy/C++/Project1/main.cpp
+++ Udemy/C++/Project1/main.cpp
@@ -1,16 +1,15 @@
 // Programmer: Aidan Fox
+// Section 4 Challenge
+// Prompt user to enter their favorite number between 1 and 100, then read from console.
 
 #include <iostream>
 
-// first project in Udemy course
-// added some external knowledge from previously working with the language.
-
-
 int main() {
 	int favoriteNumber;
-
-	std::cout << "Enter a number between 1 and 100" << std::endl;
+	std::cout << "Enter your favorite number between 1 and 100: ";
 	std::cin >> favoriteNumber;
-	std::cout << "Wow! " << favoriteNumber << " is my favorite number too!" << std::endl;
+	std::cout << "Amazing!! That's my favorite number too!" << std::endl;
+	std::cout << "No really, " << favoriteNumber <<
+		" is my favorite number!" << std::endl;
 	return 0;
 }
