12ca9528bb61fa61f7177487dffe897b2062d734
aidanfoxcsun <aidan.fox.220@my.csun.edu>
2023-06-22 14:54:32 -0700

    Section 8 Challenge Problem
    Uploading my section 8 challenge problem, in which I generate this coins need to provide accurate change efficiently.

--- C++/Section 8/Statements and Operators/main.cpp
+++ C++/Section 8/Statements and Operators/main.cpp
@@ -2,7 +2,23 @@
 *  Section 8
 *  Statements and Operators
 * 
-*  Average of 3 numbers
+*  Challenge
+* 
+*  Write a program that asks the user to enter the following:
+*  An integer representing the number of cents
+*  You may assume that the number of cents entered 
+*  is greater than or equal to zero.
+*  This program should then display how to provide change to that user.
+* 
+*  Sample run:
+* 
+*  Enter an amount in cents: 92
+*  You can provide this change as follows:
+*  Dollars: 0
+*  Quarters: 3
+*  Dimes: 1
+*  Nickels: 1
+*  Pennies: 2
 */
 
 #include <iostream>
@@ -12,20 +28,46 @@ using std::cin;
 using std::endl;
 
 int main() {
-	int total{};
-	int num1{}, num2{}, num3{};
-	const int count{ 3 };
+	const int dollarVal{ 100 }; // just for readability's sake
+	const int quarterVal{ 25 };
+	const int dimeVal{ 10 };
+	const int nickelVal{ 5 };
+	const int pennyVal{ 1 };
 
-	cout << "Enter 3 integers separated by spaces: ";
-	cin >> num1 >> num2 >> num3;
-	total = num1 + num2 + num3;
+	int userAmount{};
 
-	double average{0.0};
-	average = static_cast<double>(total) / count;
-	// average = (double)total/count;  OLD METHOD!
+	int dollar{}, quarter{}, dime{}, nickel{}, penny{};
 
-	cout << "Your three numbers are: " << num1 << ", " << num2 << ", " << num3 << endl;
-	cout << "The sum of your numbers is: " << total << endl;
-	cout << "The average of your three integers is: " << average << endl;
+	cout << "Enter an amount is cents: ";
+	cin >> userAmount;
+
+	// Probably more efficient to put this in a function
+	if (userAmount >= dollarVal) {
+		dollar = userAmount / dollarVal;
+		userAmount %= dollarVal;
+	}
+	if (userAmount >= quarterVal) {
+		quarter = userAmount / quarterVal;
+		userAmount %= quarterVal;
+	}
+	if (userAmount >= dimeVal) {
+		dime = userAmount / dimeVal;
+		userAmount %= dimeVal;
+	}
+	if (userAmount >= nickelVal) {
+		nickel = userAmount / nickelVal;
+		userAmount %= nickelVal;
+	}
+	penny = userAmount;
+
+	cout << "You can provide this change as follows: " << endl;
+	cout << "Dollars: " << dollar << endl;
+	cout << "Quarters: " << quarter << endl;
+	cout << "Dimes: " << dime << endl;
+	cout << "Nickels: " << nickel << endl;
+	cout << "Pennies: " << penny << endl;
+
 	return 0;
 }
+
+// Modulo made this challenge much more simple and time efficient to achieve than it would've been without it
